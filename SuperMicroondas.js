62fd50ea3f57c70cb49310e9ecbde9bd0d8621c2
variascristinas <cfariasdebritto@gmail.com>
2022-11-07 20:43:25 -0300

    Checkpoint

---
+++ SuperMicroondas.js
@@ -0,0 +1,80 @@
+var comida
+var tempo
+
+function Preparo (comida, tempo) {
+    if (comida ==="pipoca") {
+        if (tempo ==='10') {
+    console.log ("Prato pronto, bom apetite!!!")
+    
+     } else if (tempo >2*10) {
+    console.log ("Sua comida queimou")
+    
+     } else if (tempo >3*10) {
+    console.log ("Kabumm")
+    
+     } else if (tempo <10) {
+        console.log ("tempo insuficiente")
+     }
+    }
+    if (comida ==="carne") {
+    if (tempo ==='15') {
+        console.log ("Prato pronto, bom apetite")
+
+    } else if (tempo >2*15){
+        console.log ("Sua comida queimou")
+
+    } else if (tempo >3*15) {
+    console.log ("Kabumm")
+
+    } else if (tempo <15) {
+        console.log ("tempo insuficiente")
+}
+
+ }
+ if (comida ==="Macarrão") {
+    if (tempo ==='8') {
+        console.log ("Prato pronto, bom apetite")
+
+    } else if (tempo >2*8){
+        console.log ("Sua comida queimou")
+
+    } else if (tempo >3*8) {
+    console.log ("Kabumm")
+
+    } else if (tempo <8) {
+        console.log ("tempo insuficiente")
+}
+
+ }
+ if (comida ==="Feijão") {
+    if (tempo ==='12') {
+        console.log ("Prato pronto, bom apetite")
+
+    } else if (tempo >2*12){
+        console.log ("Sua comida queimou")
+
+    } else if (tempo >3*12) {
+    console.log ("Kabumm")
+
+    } else if (tempo <12) {
+        console.log ("tempo insuficiente")
+}
+
+ }
+
+ if (comida ==="Brigadeiro") {
+    if (tempo ==='8') {
+        console.log ("Prato pronto, bom apetite")
+
+    } else if (tempo >2*8){
+        console.log ("Sua comida queimou")
+
+    } else if (tempo >3*8) {
+    console.log ("Kabumm")
+
+    } else if (tempo <8) {
+        console.log ("tempo insuficiente")
+}
+
+ }
+}
